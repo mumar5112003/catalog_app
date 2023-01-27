a85ee86c2a92e7e3072d099a0bed5573cd4e35b1
Your Name <you@example.com>
2023-01-27 05:50:31 +0500

    Theme Extracted
    Signed-off-by: Your Name <you@example.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:catalog_app/pages/home_page.dart';
 import 'package:catalog_app/pages/login_page.dart';
 import 'package:catalog_app/utils/routes.dart';
+import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,17 +17,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+
       // initialRoute: '/login',
       routes: {
-        '/': (context) => LoginPage(),
-        MyRoute.loginRoute: (context) => LoginPage(),
-        MyRoute.homeRoute: (context) => HomePage(),
+        '/': (context) => const HomePage(),
+        MyRoute.loginRoute: (context) => const LoginPage(),
+        MyRoute.homeRoute: (context) => const HomePage(),
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,17 +5,19 @@ import '../widgets/drawer.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Catalog App"),
-      ),
-      body: Center(
-        child: Container(
-          child: const Text("Welcome to flutter app"),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 55),
+          child: Text("Catalog App"),
         ),
       ),
-      drawer: MyDrawer(),
+      body: const Center(
+        child: Text("Welcome to flutter app"),
+      ),
+      drawer: const MyDrawer(),
     );
   }
 }
